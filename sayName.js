1d107fb10483bcf6d33310f5350359fc006b7144
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-01-28 19:56:47 -0700

    complete assignment

--- sayName.js
+++ sayName.js
@@ -1,13 +1,24 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
-  //code here
+  var Person = function(name, age) {
+  	this.name = name;
+  	this.age = age;
+  }
 
 
 //Now create three instances of Person with data you make up
 
-  //code here
+  var rachel = new Person('Rachel', 28);
+  var tyler = new Person('Tyler', 30);
+  var bobby = new Person('Bobby', 26);
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
-  //code here
+  var Person = function(name, age) {
+  	this.name = name;
+  	this.age = age;
+  	this.sayName = function() {
+  		alert(this.name);
+  	}
+  }
